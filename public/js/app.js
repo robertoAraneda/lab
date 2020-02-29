d1c55cae5c4b6523a65330989c5f16bbb2cd7e6c
robaraneda <robaraneda@gmail.com>
2020-02-29 19:45:27 -0300

    app js

--- public/js/app.js
+++ public/js/app.js
@@ -117694,8 +117694,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Apache24\htdocs\Lab\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Apache24\htdocs\Lab\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Apache24\htdocs\laboratorio\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Apache24\htdocs\laboratorio\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
