d314193191b669f07a40926b5d7060fc4035ed31
Roberto Araneda Espinoza <robaraneda@gmail.com>
2020-10-17 07:56:33 -0300

    modifica migrations por valores null

--- database/migrations/2020_10_10_101102_create_minsal_details_table.php
+++ database/migrations/2020_10_10_101102_create_minsal_details_table.php
@@ -15,22 +15,22 @@ class CreateMinsalDetailsTable extends Migration
     {
         Schema::connection('mysqlGestion')->create('minsal_details', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('patient_rut');
-            $table->string('patient_name');
-            $table->string('patient_gender');
-            $table->string('patient_age');
-            $table->string('patient_phone');
-            $table->string('patient_email');
-            $table->string('patient_address');
-            $table->string('sample');
-            $table->string('result');
-            $table->dateTime('sampled_at');
-            $table->dateTime('received_at');
-            $table->dateTime('validated_at');
-            $table->string('requesting_institution_name');
-            $table->string('requesting_institution_region');
-            $table->string('processing_laboratory_name');
-            $table->string('processing_laboratory_region');
+            $table->string('patient_rut')->nullable();
+            $table->string('patient_name')->nullable();
+            $table->string('patient_gender')->nullable();
+            $table->string('patient_age')->nullable();
+            $table->string('patient_phone')->nullable();
+            $table->string('patient_email')->nullable();
+            $table->string('patient_address')->nullable();
+            $table->string('sample')->nullable();
+            $table->string('result')->nullable();
+            $table->dateTime('sampled_at')->nullable();
+            $table->dateTime('received_at')->nullable();
+            $table->dateTime('validated_at')->nullable();
+            $table->string('requesting_institution_name')->nullable();
+            $table->string('requesting_institution_region')->nullable();
+            $table->string('processing_laboratory_name')->nullable();
+            $table->string('processing_laboratory_region')->nullable();
            
         });
     }
