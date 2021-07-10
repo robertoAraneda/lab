dd324bbab6b7f35f129ec2c449f72d83b771cccc
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-07-10 12:53:04 -0400

    corregir

--- app/Exports/AnalyteExport.php
+++ app/Exports/AnalyteExport.php
@@ -69,11 +69,10 @@ class AnalyteExport implements FromCollection, WithMapping, WithHeadings, Should
                 $analyte->updatedUser->name,
                 $analyte->quantitySamplePediatric->description,
                 $analyte->quantitySampleAdult->description,
-                $analyte->analyteSampleContainer->main_analyte ?? $analyte->analyteSampleContainer->main_analyte->description,
-                $analyte->analyteSampleContainer->container->description,
-                $analyte->analyteSampleContainer->sample_collection_method->sample->description,
-                $analyte->analyteSampleContainer->sample_collection_method->collection_method->description,
-
+                $analyte->analyteSampleContainer == null ? '': $analyte->analyteSampleContainer->main_analyte == null ? '': $analyte->analyteSampleContainer->main_analyte->main_analyte,
+                $analyte->analyteSampleContainer == null ? '': $analyte->analyteSampleContainer->container == null ? '': $analyte->analyteSampleContainer->container->description,
+                $analyte->analyteSampleContainer == null ? '': $analyte->analyteSampleContainer->sample_collection_method == null ? '': $analyte->analyteSampleContainer->sample_collection_method->sample == null ? '': $analyte->analyteSampleContainer->sample_collection_method->sample->description,
+                $analyte->analyteSampleContainer == null ? '': $analyte->analyteSampleContainer->sample_collection_method == null ? '': $analyte->analyteSampleContainer->sample_collection_method->collection_method == null ? '': $analyte->analyteSampleContainer->sample_collection_method->collection_method->description,
             ];
 
     }
