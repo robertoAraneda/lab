7af1da4d6fd51761d72be73beef2ba84ebc05352
Roberto Araneda Espinoza <robaraneda@gmail.com>
2021-07-10 13:17:30 -0400

    mejora

--- app/Exports/AnalyteExport.php
+++ app/Exports/AnalyteExport.php
@@ -69,9 +69,9 @@ class AnalyteExport implements FromCollection, WithMapping, WithHeadings, Should
                 $analyte->updatedUser->name,
                 $analyte->quantitySamplePediatric->description,
                 $analyte->quantitySampleAdult->description,
-                $analyte->analyteSampleContainer->main_analyte == null ? '': $analyte->analyteSampleContainer->main_analyte->description,
+                $analyte->analyteSampleContainer->mainAnalyte == null ? '': $analyte->analyteSampleContainer->main_analyte->description,
                 $analyte->analyteSampleContainer->container->description,
-                $analyte->analyteSampleContainer->sample_collection_method,
+                $analyte->analyteSampleContainer->sampleCollectionMethod,
                 $analyte->analyteSampleContainer
             ];
 
